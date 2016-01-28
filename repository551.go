d64e5152c9c77e657759a412d95b4f85bb4fe4fb
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-28 11:42:14 +0900

    refs #3 Find メソッドを実装する

--- repository551.go
+++ repository551.go
@@ -1,5 +1,11 @@
 package repository551
 
+import (
+	"errors"
+	"github.com/go51/model551"
+	"github.com/go51/mysql551"
+)
+
 type Repository struct{}
 
 var repositoryInstance *Repository
@@ -12,4 +18,30 @@ func Load() *Repository {
 	repositoryInstance = &Repository{}
 
 	return repositoryInstance
+}
+
+func (r *Repository) Find(db *mysql551.Mysql, mInfo *model551.ModelInformation, id int64) interface{} {
+
+	sql := mInfo.SqlInformation.Select + " AND `" + mInfo.TableInformation.PrimaryKey + "` = ?"
+	param := []interface{}{id}
+
+	rows := db.Query(sql, param...)
+	defer rows.Close()
+	if !rows.Next() {
+		return nil
+	}
+
+	model := mInfo.NewFuncPointer()
+	modelScan, ok := model.(model551.ScanInterface)
+	if !ok {
+		panic(errors.New("Not found: 'T.Scan(rows *sql.rows) error' method"))
+	}
+
+	err := modelScan.Scan(rows)
+	if err != nil {
+		panic(err)
+	}
+
+	return model
+
 }
--- repository551_test.go
+++ repository551_test.go
@@ -1,9 +1,52 @@
 package repository551_test
 
 import (
+	"database/sql"
+	"github.com/go51/model551"
+	"github.com/go51/mysql551"
 	"github.com/go51/repository551"
+	"os"
 	"testing"
+	"time"
 )
+
+var db *mysql551.Mysql
+var mm *model551.Model
+
+func TestMain(m *testing.M) {
+
+	dbConfig := &mysql551.Config{
+		Host:     "tcp(localhost:3306)",
+		User:     "root",
+		Password: "",
+		Database: "repository551",
+	}
+	db = mysql551.New(dbConfig)
+	db.Open()
+	defer db.Close()
+
+	// Truncate
+	sql := "TRUNCATE TABLE sample"
+	_, _ = db.Exec(sql)
+	sql = "TRUNCATE TABLE sample_delete"
+	_, _ = db.Exec(sql)
+
+	// Insert
+	sql = "INSERT INTO sample (`group_id`, `name`, `description`) VALUES (?, ?, ?)"
+	_, _ = db.Exec(sql, 2, "pubapp.biz_1", "domain_1")
+	_, _ = db.Exec(sql, 1, "pubapp.biz_2", "domain_2")
+	_, _ = db.Exec(sql, 2, "pubapp.biz_3", "domain_3")
+	_, _ = db.Exec(sql, 2, "pubapp.biz_4", "domain_4")
+	_, _ = db.Exec(sql, 3, "pubapp.biz_5", "domain_5")
+
+	mm = model551.Load()
+	mm.Add(NewSampleModel, NewSampleModelPointer)
+
+	code := m.Run()
+
+	os.Exit(code)
+
+}
 
 func TestLoad(t *testing.T) {
 	m1 := repository551.Load()
@@ -22,4 +65,77 @@ func BenchmarkLoad(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = repository551.Load()
 	}
+}
+
+type Sample struct {
+	Id          int64
+	GroupId     int64
+	Name        string
+	Description string
+	DeletedAt   time.Time `db_delete:"true"`
+}
+
+func NewSampleModel() interface{} {
+	return Sample{}
+}
+
+func NewSampleModelPointer() interface{} {
+	return &Sample{}
+}
+
+func (m *Sample) Scan(rows *sql.Rows) error {
+
+	err := rows.Scan(
+		&m.Id,
+		&m.GroupId,
+		&m.Name,
+		&m.Description,
+	)
+
+	return err
+}
+
+func TestFind(t *testing.T) {
+
+	repo := repository551.Load()
+	mtSample := mm.Get("Sample")
+
+	ret := repo.Find(db, mtSample, 1)
+	if ret == nil {
+		t.Errorf("取得に失敗しました。")
+	}
+
+	sample, _ := ret.(*Sample)
+	if sample.Id != 1 {
+		t.Errorf("取得に失敗しました。")
+	}
+	if sample.GroupId != 2 {
+		t.Errorf("取得に失敗しました。")
+	}
+	if sample.Name != "pubapp.biz_1" {
+		t.Errorf("取得に失敗しました。")
+	}
+	if sample.Description != "domain_1" {
+		t.Errorf("取得に失敗しました。")
+	}
+
+	ret = repo.Find(db, mtSample, 2)
+	if ret == nil {
+		t.Errorf("取得に失敗しました。")
+	}
+
+	sample, _ = ret.(*Sample)
+	if sample.Id != 2 {
+		t.Errorf("取得に失敗しました。")
+	}
+	if sample.GroupId != 1 {
+		t.Errorf("取得に失敗しました。")
+	}
+	if sample.Name != "pubapp.biz_2" {
+		t.Errorf("取得に失敗しました。")
+	}
+	if sample.Description != "domain_2" {
+		t.Errorf("取得に失敗しました。")
+	}
+
 }
